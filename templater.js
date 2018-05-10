7be4e20bd396fe8bf971e3d9b8aaa27f80c9bf5c
Stanislav Gurin <sgurin@justcoded.com>
2018-05-10 16:46:03 +0300

    Stage-1 has been completed

--- templater.js
+++ templater.js
@@ -1,1 +1,22 @@
-console.log(1)
+/**
+ * Replace any you tag with custom tag that you have provided
+ * @example
+ * // returns <span>test</span>
+ * Templater.run("<div>test</div>", "<span>test</span>")
+ */
+
+let Templater = {
+	run: (tag, customTag) => {
+
+		let elements = Array.from(document.getElementsByTagName(tag));
+
+		for (let i = 0; i <= elements.length; i++) {
+
+			elements[i].outerHTML= customTag;
+
+		}
+		
+	}
+}
+
+Templater.run("bootstrap_button", "<button class=\"btn btn-default\" type=\"submit\">Some Text</button>");
